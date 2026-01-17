e3901be65f0d6258ca2647eb8087343bb9989f11
Macky Japzon <mackyjapzon@cmos.local>
2026-01-18 03:28:59 +0800

    Design implementation improvements and fixes

--- app/components/AccountForm.tsx
+++ app/components/AccountForm.tsx
@@ -293,7 +293,6 @@ export default function AccountForm({
               height: 40,
               alignItems: "center",
               justifyContent: "center",
-              borderRadius: 8,
               marginLeft: 8,
             }}
           >
